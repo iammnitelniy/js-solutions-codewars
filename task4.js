cf53cc401caa42e8fa040c49a02ca212674cf04e
abc <abc@example.com>
2023-02-24 15:17:14 +0600

    boom

---
+++ task4.js
@@ -0,0 +1,6 @@
+function areYouPlayingBanjo(name) {
+    // Implement me
+    
+    name[0] === "r" || name[0] === "R" ? name +=" plays banjo" :  name += " does not play banjo"
+    return name
+  }
